6a421170aae309bf3f5d8b08f7204f43bdb630cb
TheGloriousMoron <128844334+TheGloriousMoron@users.noreply.github.com>
2024-10-04 15:22:37 +0200

    did testing, image making don't work ):

--- src/main.c
+++ src/main.c
@@ -3,15 +3,15 @@
 int main() {
     srand(time(NULL));
 
-    uint32_t grid_size = 2;
-    uint32_t max_particles = 2 * 2;
-    uint32_t iteration_max = 256;
+    uint32_t grid_size = 128;
+    uint32_t max_particles = 100;
+    uint32_t iteration_max = 2560;
 
     grid_t *grid = grid_init(grid_size, max_particles, iteration_max);
 
     grid_run_simulation(grid);
 
-    save_grid(grid, "test2.png");
+    save_grid(grid, "SIZE-128_PART-100.png");
 
     grid_free(grid);
 
